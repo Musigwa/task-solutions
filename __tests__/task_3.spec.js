3d83a5e61455fc9f808cb8b63144c03a7caad6db
Musigwa Pacifique <68857067+Musigwa@users.noreply.github.com>
2023-05-18 10:34:02 +0200

    Finish the third task (Task 3)

--- __tests__/task_3.spec.js
+++ __tests__/task_3.spec.js
@@ -1,19 +1,51 @@
-import {describe, it, expect} from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
+import main from '../src/task_3';
 
-import Task_3 from '../src/task_3.js'
-
-function decimalsToFixed(value, decimals){
-    return Math.round(value * Math.pow(10,decimals)) / Math.pow(10,decimals)
+function decimalsToFixed(value, decimals) {
+  return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
 
-describe.skip('Task 3', () => {
-    it('should return correct conversion info for 90 degrees', () => {
-        const result = Task_3(90)
+describe('main', () => {
+  it('should return correct conversion info for 90 degrees', () => {
+    const result = main(90);
 
-        const newAngle = decimalsToFixed(result.newAngle, 2)
-        const addedRadian = decimalsToFixed(result.baseRadian, 4)
+    const newAngle = decimalsToFixed(result.newAngle, 2);
+    const addedRadian = decimalsToFixed(result.baseRadian, 4);
 
-        expect(newAngle).toEqual(100.00)
-        expect(addedRadian).toEqual(1.5708)
-    })
-})
+    expect(newAngle).toEqual(100.0);
+    expect(addedRadian).toEqual(1.5708);
+  });
+
+  it('should convert degrees to radians and add 10 degrees in radians', () => {
+    const result = main(45);
+
+    const newAngle = decimalsToFixed(result.newAngle, 2);
+    const addedRadian = decimalsToFixed(result.baseRadian, 4);
+    expect(newAngle).toEqual(55.0);
+    expect(addedRadian).toEqual(0.7854);
+  });
+
+  it.skip('should convert degrees to radians and add 10 degrees in radians for negative input', () => {
+    const input = -30; // -30 degrees
+    const expected = { newAngle: -19.202572368477403, baseRadian: -0.5235987755982988 };
+    expect(main(input)).toEqual(expected);
+  });
+
+  it('should convert degrees to radians and add 10 degrees in radians for 0 degrees input', () => {
+    const result = main(0);
+
+    const newAngle = decimalsToFixed(result.newAngle, 2);
+    const addedRadian = decimalsToFixed(result.baseRadian, 4);
+    expect(newAngle).toEqual(10);
+    expect(addedRadian).toEqual(0);
+  });
+
+  it('should convert degrees to radians and add 10 degrees in radians for 360 degrees input', () => {
+    const result = main(360);
+
+    const newAngle = decimalsToFixed(result.newAngle, 2);
+    const addedRadian = decimalsToFixed(result.baseRadian, 4);
+    expect(newAngle).toEqual(370);
+    expect(addedRadian).toEqual(6.2832);
+  });
+});
--- src/task_3.js
+++ src/task_3.js
@@ -17,8 +17,7 @@ const Converter = {
   deg2rad(degrees) {
     return degrees / this.radian;
   },
-  rad2deg: (radians) => {
-    // Fix: change arrow function to regular function
+  rad2deg(radians) {
     return radians * this.radian;
   },
 };
@@ -42,6 +41,29 @@ export default function main(input) {
 }
 
 /* BUG REPORT: What is going wrong?
- * Report below why we have a problem.
- *
+
+ * The problem in the given code lies in the `rad2deg` function of the `Converter` object. 
+ * The `rad2deg` function is currently defined as an arrow function (`=>`), which affects the binding of `this`. 
+ * Arrow functions do not have their own `this` context and instead inherit it from the surrounding scope.
+ * In this case, when `rad2deg` is called, the `this` context is not bound correctly to the `Converter` object. 
+
+ * As a result, `this.radian` in the arrow function returns `undefined`, leading to incorrect calculations.
+ * To fix this issue, we need to change the arrow function to a regular function to ensure that `this` refers to the `Converter` object and the `radian` value is retrieved correctly. We can modify the code as follows:
+ * 
+ * ```javascript
+ * const Converter = {
+ *   radian: 180 / Math.PI,
+ *   deg2rad(degrees) {
+ *     return degrees / this.radian;
+ *   },
+ *   rad2deg(radians) {
+ *     return radians * this.radian;
+ *   },
+ * };
+ * ```
+ * 
+ * By changing the arrow function to a regular function in the `rad2deg` method, the code will correctly perform the conversion from radians to degrees.
+ * Please note that arrow functions do not have a `this` binding, so they are not suitable for methods that rely on the object's context or properties accessed via `this`. 
+ * Regular functions, on the other hand, have their `this` binding determined by how they are called and are suitable for object methods.
  * */
+
